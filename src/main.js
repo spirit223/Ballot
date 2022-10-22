5829bc8595c8b0069ddf78e6977f0a6216f577dd
spirit223 <2235569493@qq.com>
2022-10-22 22:04:05 +0800

    修改布局

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 
-import './assets/main.css'
+// import './assets/main.css'
 
 const app = createApp(App)
 
